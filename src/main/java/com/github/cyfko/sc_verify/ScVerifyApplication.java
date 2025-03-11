af37ba85016e9241735d704e8f8d6bf62f034dcf
unknown <frank.kossi@sprint-pay.com>
2025-03-11 16:07:36 +0100

    first comit

---
+++ src/main/java/com/github/cyfko/sc_verify/ScVerifyApplication.java
@@ -0,0 +1,13 @@
+package com.github.cyfko.sc_verify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScVerifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScVerifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cyfko/sc_verify/ScVerifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cyfko.sc_verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScVerifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
